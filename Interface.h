d6d37979f5a7445912ae08195a7a3be1307928b5
Zach <zphillips080@anselm.edu>
2021-05-03 10:37:09 -0400

    Project - Removes Interface Class and Implements UI Functions

--- Interface.h
+++ Interface.h
@@ -7,25 +7,6 @@
 
 class Interface{
 public:
-    std::string pos_name;
-    std::vector<int> skill_pref;
-    std::vector<std::string> pos_skills;
-    std::string skill;
-    std::string pref;
-    std::string userChoice;
-    std::string jobFileName;
-    std::ofstream jobFile;
-    std::string line;
-    int lineCounter = 0;
-    std::string currentWord;
-    int spaceCounter = 0;
-    std::string resumeFileName;
-    std::vector<std::string> resumeFileNameVector;
-    std::fstream resumeFile;
-    Interface();
-    void getJobInfo();
-    void getResumeInfo();
-
 
 };
 
